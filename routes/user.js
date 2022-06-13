5949d5d521faf8389d9c836c108fef4fac8f61ff
shahardc <81380081+shahardc@users.noreply.github.com>
2022-06-13 23:45:50 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -62,7 +62,7 @@ router.get('/favorites', async (req,res,next) => {
  router.post('/watched', async (req,res,next) => {
   try{
     const user_id = req.session.user_id;
-    const recipe_id = req.body.recipeId;
+    const recipe_id = req.body.recipe_id;
     await user_utils.markAsWatched(user_id,recipe_id);
     res.status(200).send("The Recipe successfully saved as watched");
     } catch(error){
@@ -76,11 +76,11 @@ router.get('/favorites', async (req,res,next) => {
 router.get('/watched', async (req,res,next) => {
   try{
     const user_id = req.session.user_id;
-    let favorite_recipes = {};
     const recipes_id = await user_utils.getWatchedRecipes(user_id);
-    let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
-    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+    const results = recipes_id;
+    //let recipes_id_array = [];
+    //recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    //const results = await recipe_utils.getRecipesPreview(recipes_id_array);
     res.status(200).send(results);
   } catch(error){
     next(error); 
