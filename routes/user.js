2cc51000aca3db8983cefd1f1724e5d09fd914fb
shahardc <81380081+shahardc@users.noreply.github.com>
2022-06-14 23:30:16 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -86,4 +86,64 @@ router.get('/watched', async (req,res,next) => {
     next(error); 
   }
 });
+
+
+/**
+ * This path returns the 3 last watched recipes that were saved by the logged-in user
+ */
+router.get('/watched3', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const recipes_id = await user_utils.getWatchedRecipes3(user_id);
+    const results = recipes_id;
+    //let recipes_id_array = [];
+    //recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    //const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+    res.status(200).send(results);
+  } catch(error){
+    next(error); 
+  }
+});
+
+/**
+ * This path gets body with recipe details and save this recipe in the personal recipes of the logged-in user
+ */
+router.post('/personal', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const recipe_id = req.body.recipe_id;
+    const picture = req.body.picture;
+    const name = req.body.name;
+    const time_to_prepare = req.body.time_to_prepare;
+    const vegan = req.body.vegan;
+    const vegeterian = req.body.vegeterian;
+    const ingridients = req.body.ingridients;
+    const instructions = req.body.instructions;
+    const number_of_servings = req.body.number_of_servings;
+    const glutenFree = req.body.glutenFree;
+    await user_utils.addPersonalRecipe(user_id,recipe_id,picture,name,time_to_prepare,vegan,vegeterian,ingridients,instructions,number_of_servings,glutenFree);
+    res.status(200).send("The Personal Recipe was successfully saved");
+    } catch(error){
+    next(error);
+  }
+})
+
+/**
+ * This path returns the 3 last watched recipes that were saved by the logged-in user
+ */
+ router.get('/personal', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const recipes_id = await user_utils.getPersonalRecipes(user_id);
+    const results = recipes_id;
+    //let recipes_id_array = [];
+    //recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    //const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+    res.status(200).send(results);
+  } catch(error){
+    next(error); 
+  }
+});
+
+
 module.exports = router;
