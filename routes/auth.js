c1f5aa0758d2c0e86ac2ad862f7a416b6706e80a
abeksis93 <abeksis@post.bgu.ac.il>
2022-08-17 23:47:37 +0300

    update

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,6 @@ router.post("/Register", async (req, res, next) => {
     }
     let users = [];
     users = await DButils.execQuery("SELECT username from users");
-
     if (users.find((x) => x.username === user_details.username))
       throw { status: 409, message: "Username taken" };
 
